ec40135ef6ee9cc8caedc79b7d70e3a8d9107c1a
Zack Brady <zackbrady123@gmail.com>
2023-06-26 19:10:45 -0400

    updated aws ecr terraform

--- terraform/aws-ecr-rancher-tf/modules/ecr/output.tf
+++
@@ -1,9 +0,0 @@
-output "repository_url" {
-  value = aws_ecr_repository.ecr_repository.repository_url
-  description = "The url of the repository (aws_account_id.dkr.ecr.region.amazonaws.com/repositoryName)."
-}
-
-output "registry_id" {
-  value = aws_ecr_repository.ecr_repository.registry_id
-  description = "The registry id where the repository was created in aws."
-}
--- terraform/aws-ecr-rancher-tf/providers.tf
+++ terraform/aws-ecr-rancher-tf/providers.tf
@@ -10,7 +10,9 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region      = var.region
+  access_key  = var.access_key
+  secret_key  = var.secret_key
 
   default_tags {
     tags = {
