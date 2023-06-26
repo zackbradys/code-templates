a8dd33fded09d547c61b6db16341455abe2e3877
Zack Brady <zackbrady123@gmail.com>
2023-06-26 00:22:04 -0400

    finally working... wow

--- terraform/aws-rke2-auto-tf/route53.tf
+++ terraform/aws-rke2-auto-tf/route53.tf
@@ -4,21 +4,28 @@
 #  private_zone = false
 #  comment = "AWS RKE2 Route53 Hosted Zone"
 #}
+# zone_id = aws_route53_zone.aws_rke2_zone.zone_id
 
 resource "aws_route53_record" "aws_rke2_record_rke2" {
-  #zone_id     = aws_route53_zone.aws_rke2_zone.zone_id
-  zone_id     = "Z089762637TAJS9TTNWZL"
+  zone_id     = "Z01241842LH4ZO7IUQCTP"
   name        = ""
   type        = "A"
-  ttl         = 300
-  records     = [aws_elb.aws_rke2_lb.dns_name]
+  alias {
+    name                    = "${aws_elb.aws_rke2_lb.dns_name}"
+    zone_id                 = "${aws_elb.aws_rke2_lb.zone_id}"
+    evaluate_target_health  = false
+  }
+  depends_on  = [aws_elb.aws_rke2_lb]
 }
 
 resource "aws_route53_record" "aws_rke2_record_ingress" {
-  # zone_id = aws_route53_zone.aws_rke2_zone.zone_id
-  zone_id     = "Z089762637TAJS9TTNWZL"
+  zone_id     = "Z01241842LH4ZO7IUQCTP"
   name        = "*"
-  type        = "CNAME"
-  ttl         = 300
-  records     = [aws_elb.aws_rke2_ingress_lb.dns_name]
+  type        = "A"
+  alias {
+    name                    = "${aws_elb.aws_rke2_ingress_lb.dns_name}"
+    zone_id                 = "${aws_elb.aws_rke2_ingress_lb.zone_id}"
+    evaluate_target_health  = false
+  }
+  depends_on  = [aws_elb.aws_rke2_ingress_lb]
 }
--- terraform/aws-rke2-tf/outputs.tf
+++ terraform/aws-rke2-tf/outputs.tf
@@ -1,34 +1,4 @@
-output "vpc_id" {
-  value       = [aws_vpc.aws_rke2_vpc.id]
-  description = "VPC ID for the AWS RKE2 cluster"
-}
-
-output "public_subnet_ids" {
-  value       = [aws_subnet.aws_rke2_public_subnet1.id, aws_subnet.aws_rke2_public_subnet2.id, aws_subnet.aws_rke2_public_subnet3.id]
-  description = "Public Subnet IDs for the AWS RKE2 cluster"
-}
-
-output "private_subnet_ids" {
-  value       = [aws_subnet.aws_rke2_private_subnet1.id, aws_subnet.aws_rke2_private_subnet2.id, aws_subnet.aws_rke2_private_subnet3.id]
-  description = "Private Subnet IDs for the AWS RKE2 cluster"
-}
-
-output "instance_ids_control" {
-  value       = ["${aws_instance.aws_ec2_instance_control.*.id}"]
-  description = "Instance IDs for the Control Nodes in the AWS RKE2 cluster"
-}
-
-output "instance_ids_worker" {
-  value       = ["${aws_instance.aws_ec2_instance_worker.*.id}"]
-  description = "Instance IDs for the Worker Nodes in the AWS RKE2 cluster"
-}
-
-output "instance_ips_control" {
-  value       = ["${aws_instance.aws_ec2_instance_control.*.private_ip}"]
-  description = "Instance IPs for the Control Nodes in the AWS RKE2 cluster"
-}
-
-output "instance_ips_worker" {
-  value       = ["${aws_instance.aws_ec2_instance_worker.*.private_ip}"]
-  description = "Instance IPs for the Worker Nodes in the AWS RKE2 cluster"
+output "timestamp" {
+  value       = [timestamp()]
+  description = "Create/Update Timestamp"
 }
