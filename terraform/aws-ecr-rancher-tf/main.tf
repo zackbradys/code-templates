fb71c8512a31d805591ed00511418b69f007c351
Zack Brady <zackbrady123@gmail.com>
2023-06-27 14:37:56 -0400

    updated ecr terraform

--- terraform/aws-ecr-rancher-tf/main.tf
+++ terraform/aws-ecr-rancher-tf/main.tf
@@ -7,10 +7,44 @@ resource "aws_ecr_repository" "aws_ecr_repository" {
 
   image_scanning_configuration {
     scan_on_push        = var.scan_on_push
-  }  
+  }
 }
 
-resource "aws_ecr_lifecycle_policy" "aws_ecr_policy" {
+resource "aws_ecr_repository_policy" "aws_ecr_repo_policy" {
+  for_each             = var.name
+  repository           = aws_ecr_repository.aws_ecr_repository[each.key].name
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "aws ecr image policy (allowall)",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:PutImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload",
+                "ecr:DescribeRepositories",
+                "ecr:GetRepositoryPolicy",
+                "ecr:ListImages",
+                "ecr:DeleteRepository",
+                "ecr:BatchDeleteImage",
+                "ecr:SetRepositoryPolicy",
+                "ecr:DeleteRepositoryPolicy"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_ecr_lifecycle_policy" "aws_ecr_expire_policy" {
   for_each             = var.name
   repository           = aws_ecr_repository.aws_ecr_repository[each.key].name
 
