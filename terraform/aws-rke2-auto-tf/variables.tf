a752341b03c0b540663fa3c0258b5090689c8430
Zack Brady <zackbrady123@gmail.com>
2023-07-25 12:52:45 -0400

    upgraded to rke2 v1.25

--- terraform/aws-rke2-auto-tf/variables.tf
+++ terraform/aws-rke2-auto-tf/variables.tf
@@ -42,7 +42,7 @@ variable "token" {
 }
 
 variable "vRKE2" {
-  default     = "v1.24"
+  default     = "v1.25"
   type        = string
   description = "(Required) The RKE2 Version to use for the clusters(s)."
 }
