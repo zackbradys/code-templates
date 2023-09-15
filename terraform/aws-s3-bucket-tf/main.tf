ef867f9449c3c75080abbf989d6824bd1fea85a6
Zack Brady <zackbrady123@gmail.com>
2023-09-15 09:12:59 -0400

    updated terraform for aws s3 code

--- terraform/aws-s3-bucket-tf/main.tf
+++ terraform/aws-s3-bucket-tf/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket              = "rancher-federal-repo"
+  bucket              = var.bucket_name
   object_lock_enabled = false
   force_destroy       = true
 
